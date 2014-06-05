edb7bdfcba34856ce33d507f42a3c7f3d31e1dbd
Bret Copeland <bret@atlantisflight.org>
2014-06-04 23:44:13 -0400

    Rename HostConfig->DaemonConfig and ClientConfig->ApiConfig.

--- test/helpers/common.js
+++ test/helpers/common.js
@@ -12,7 +12,7 @@ Common.api = null;
 Common.suiteSetup = function (done)
 {
 	if (IN_PROCESS && !daemon)
-		daemon = new Oshi.Daemon(new Oshi.Config.HostConfig());
+		daemon = new Oshi.Daemon(new Oshi.Config.DaemonConfig());
 
 	Oshi.Api.createApi(function (error, a)
 	{
